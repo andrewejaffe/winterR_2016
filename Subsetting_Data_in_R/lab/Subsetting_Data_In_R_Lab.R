8051a275be5c0fc80baf7ef6c088647ef5711725
andrewejaffe <andrewejaffe@gmail.com>
2016-01-05 10:28:40 -0500

    change name of files

--- Subsetting_Data_in_R/lab/Subsetting_Data_In_R_Lab.R
+++ Subsetting_Data_in_R/lab/Subsetting_Data_In_R_Lab.R
@@ -3,7 +3,6 @@
 # 1/5/2016
 ####################
 
-
 ## In this lab you can use the interactive console to explore 
 ## 	  but please record your commands here.  
 ## Remember anything you type here can be "sent" to the console with 
